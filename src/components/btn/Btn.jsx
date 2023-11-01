7c2517ba6879883bd4e57a67eb12d9c8f7da8498
sohailahmad-dev <sohailahmaddev@gmail.com>
2023-11-02 02:07:54 +0500

    add wishlist modal

--- src/components/btn/Btn.jsx
+++ src/components/btn/Btn.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Btn.css'
 
-export default function Btn({ label, onClick, className, style }) {
+export default function Btn({ label, onClick, className, style, icon }) {
     return (
-        <button onClick={onClick} className={className ?? 'button'} style={style} >{label}</button>
+        <button onClick={onClick} className={className ?? 'button'} style={style} >{icon}{label}</button>
     )
 }
--- src/screens/wishlist/wishlistModal/WishlistModal.jsx
+++ src/screens/wishlist/wishlistModal/WishlistModal.jsx
@@ -1,20 +1,28 @@
-import { Modal } from '@mui/material'
+import { Grid, Modal } from '@mui/material'
 import './WishlistModal.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CancelIcon from '@mui/icons-material/Cancel';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import Btn from '../../../components/btn/Btn';
 
-export default function WishlistModal({ open = true, }) {
+export default function WishlistModal({ open }) {
+    let [isOpen, setIsOpen] = useState(false)
+
+    useEffect(() => {
+        setIsOpen(open)
+    }, [open])
+
     return (
         <div>
-            <Modal open={open}>
+            <Modal open={isOpen}>
                 <div className='wishlist-modal-style' >
                     <div className='wishlist-modal-content'>
-                        <Grid container spacing={1}>
+                        <Grid container spacing={3}>
                             <Grid item xs={12}>
                                 <div className="wishlist-modal-header">
                                     <div className="wishlist-modal-heading">
                                         Detalhes do pedido
-                                        <div>
+                                        <div onClick={() => setIsOpen(false)}>
                                             <CancelIcon sx={{ fontSize: 40 }} />
                                         </div>
                                     </div>
@@ -25,7 +33,87 @@ export default function WishlistModal({ open = true, }) {
                                 </div>
                             </Grid>
                             <Grid item md={6} xs={12}>
-                                <div className="wishlist-modal-subHeading">Dados:</div>
+                                <div className="wishlist-modal-body">
+                                    <div className="wishlist-modal-subHeading">Dados:</div>
+                                    <div>
+                                        <div className="wishlist-text-heading">Nome:</div>
+                                        <div className="wishlist-text-content">Beltrano da silva</div>
+                                    </div>
+                                    <div>
+                                        <div className="wishlist-text-heading">E-mail:</div>
+                                        <div className="wishlist-text-content">Beltranodasilva@gmail.com</div>
+                                    </div>
+                                    <div>
+                                        <div className="wishlist-text-heading">Telefone:</div>
+                                        <div className="wishlist-text-content">46 99234-2349</div>
+                                    </div>
+                                    <Btn
+                                        icon={<WhatsAppIcon />}
+                                        label={'Voltar ao inicio'}
+                                        onClick={() => { }}
+                                        style={{ width: "100%", height: "45px", background: '#029711', margin: '35px 0px' }}
+                                    />
+                                    <div className="wishlist-modal-subHeading">Dados da mudança:</div>
+                                    <div>
+                                        <div className="wishlist-text-heading1">Origem:</div>
+                                        <div className="wishlist-text-content">Rio de janeiro / RJ</div>
+                                    </div>
+                                    <div>
+                                        <div className="wishlist-text-heading1">Destino:</div>
+                                        <div className="wishlist-text-content">Juazeiro / BA</div>
+                                    </div>
+                                    <div>
+                                        <div className="wishlist-text-heading1">Data da mudança:</div>
+                                        <div className="wishlist-text-content">22/02/2024</div>
+                                    </div>
+                                    <div className="wishlist-text-heading1">Descrição:</div>
+                                    <div className="wishlist-text-content1">Lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit,</div>
+
+                                </div>
+
+                            </Grid>
+                            <Grid item md={6} xs={12}>
+                                <div className="wishlist-modal-body">
+                                    <div className="wishlist-text-heading1">Descrição:</div>
+                                    <div className="wishlist-text-content1">Lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ipsum dolor sit,</div>
+                                    <div className="wishlist-text-heading1" style={{ marginTop: '25px' }}>
+                                        Listagem:
+                                    </div>
+                                    <div >
+                                        <div className="wishlist-text-heading" style={{ fontWeight: 'bold' }}>Item</div>
+                                        <div className="wishlist-text-content" style={{ fontWeight: 'bold' }}>Quantitade</div>
+                                    </div>
+                                    <div className='wishlist-modal-underline-item'>
+                                        <div className="wishlist-text-heading">Cadeira</div>
+                                        <div className="wishlist-text-content">12</div>
+                                    </div>
+                                    <div className='wishlist-modal-underline-item'>
+                                        <div className="wishlist-text-heading">Sofa</div>
+                                        <div className="wishlist-text-content">02</div>
+                                    </div>
+                                    <div className='wishlist-modal-underline-item'>
+                                        <div className="wishlist-text-heading">Estantes</div>
+                                        <div className="wishlist-text-content">08</div>
+                                    </div>
+                                    <div className='wishlist-modal-underline-item'>
+                                        <div className="wishlist-text-heading">Mesas</div>
+                                        <div className="wishlist-text-content">22</div>
+                                    </div>
+                                    <br />
+                                    <div className='wishlist-modal-underline-item2'>
+                                        <div className="wishlist-text-heading2">Horário preferencial para a mudança:</div>
+                                        <div className="wishlist-text-content2">Não</div>
+                                    </div>
+                                    <div className='wishlist-modal-underline-item2'>
+                                        <div className="wishlist-text-heading2">Necessidade de montagem/desmonstagem:</div>
+                                        <div className="wishlist-text-content2">Não</div>
+                                    </div>
+                                    <div className='wishlist-modal-underline-item2'>
+                                        <div className="wishlist-text-heading2">Restrição ou taxa de trânsito:</div>
+                                        <div className="wishlist-text-content2">Sim - R$15</div>
+                                    </div>
+
+                                </div>
 
                             </Grid>
                         </Grid>
