83e7a37688edcf6e0108d364ae53bd9d052b4609
hackedzero7 <121494420+hackedzero7@users.noreply.github.com>
2023-11-01 22:12:28 +0500

    remaining success page of form

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import {getFirestore} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage';
 const firebaseConfig = {
   apiKey: "AIzaSyBR_XbSTiGnimNRBJAcSuUNcPmkoPcGREA",
   authDomain: "frete-a8155.firebaseapp.com",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
